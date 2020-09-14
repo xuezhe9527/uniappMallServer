583f5cae5a652d1d00862f7aa15dc6483c067d83
xuezhe <18202969264@163.com>
2020-09-14 20:39:18 +0800

    uniapp商城后台主分支第一天提交，搭建基本服务器，查詢首页导航栏

---
+++ server.js
@@ -0,0 +1,28 @@
+// 1引入
+const Koa = require("koa")
+const KoaRouter = require("koa-router")
+
+// 2创建APP实例
+const app = new Koa()
+
+//4创建路由器对象，使用中间件，注册路由
+const router = new KoaRouter()
+app.use(router.routes())
+   .use(router.allowedMethods())
+ 
+const testData = require('./datas/index.json')
+router.get('/getIndexData',function(ctx,next){
+	// console.log("已到达test")
+	ctx.body = testData
+	
+})
+
+
+// 3监听服务器启动
+app.listen("3003",function(err){
+	if(err){
+		console.log("服务器启动失败");
+	}else{
+		console.log("服务器启动成功，端口号3003")
+	}
+})
